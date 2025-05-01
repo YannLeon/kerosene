00c37d9b167da6d5efab6f262a007958e8a9637a
Yann LEON <yannleon@hotmail.fr>
2025-05-01 21:41:19 +0200

    change player physcics

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,25 +1,26 @@
 extends CharacterBody2D
 
+const UP_ACCELERATION = -800.0
+const GRAVITY = 800.0
+const MAX_UP_SPEED = -400.0
+const MAX_DOWN_SPEED = 600.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+const MAX_ROTATION_UP = -30.0
+const MAX_ROTATION_DOWN = 30.0
+const ROTATION_SPEED = 6.0
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept"):
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	if Input.is_action_pressed("ui_accept"):
+		velocity.y += UP_ACCELERATION * delta
 	else:
-		velocity.x = move_toward(1, 0, SPEED)
+		velocity.y += GRAVITY * delta
+
+	velocity.y = clamp(velocity.y, MAX_UP_SPEED, MAX_DOWN_SPEED)
 
 	move_and_slide()
+
+	# ⛔ Prevent flying above y = 0
+	position.y = max(position.y, 0)
+
+	var target_angle = clamp(velocity.y / MAX_DOWN_SPEED, -1.0, 1.0) * MAX_ROTATION_DOWN
+	rotation_degrees = lerp(rotation_degrees, target_angle, ROTATION_SPEED * delta)
