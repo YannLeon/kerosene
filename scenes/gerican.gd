b86dda58a65a6848fac5d0da10edfff574e4af57
Yann LEON <yannleon@hotmail.fr>
2025-05-02 22:25:55 +0200

    spawn gerican

--- scenes/gerican.gd
+++ scenes/gerican.gd
@@ -1,17 +1,21 @@
 extends Node2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+var collected: bool = false
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	animation_player.animation_finished.connect(_on_animation_finished)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
 func _on_area_2d_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	if collected:
+		return
+	collected = true
 	animation_player.play("collect")
 	FuelStore.add_fuel(50)
+
+func _on_animation_finished(anim_name: String) -> void:
+	if anim_name == "collect":
+		queue_free()
